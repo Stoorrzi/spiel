39bd1cb7736c8dd9adf867e075b70e434e3626dd
ChristianNagel99 <chris.nagel99@gmail.com>
2024-01-11 18:07:53 +0100

    story changes

--- src/Game/Prolog.js
+++ src/Game/Prolog.js
@@ -6,17 +6,18 @@ export default function Prolog({setProlog, weiter}) {
               <div className="CoverDiv"></div>
               <img className="IMG2" src={"/images/prolog.svg"} alt="prolog" />
               <div className="D12">
-                <h1 className="H11">Prolog</h1>
+                <h1 className="H11"><br /></h1>
                 <p className="P11">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                  nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-                  erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                  et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                  Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-                  sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                  et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                  accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                  no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                Hallo Agent,
+                wir haben Neugikeiten für dich. Wir haben soeben eine Nachricht von einem Planeten 
+                am Rande der Galaxy bekommen, allerdings ist sie verschlüsselt.
+                <br />
+                Wir brauchen deine Hilfe um die Nachricht zu entziffern!
+                <br />
+                <br />
+                <br />
+                <br />
+                Interstellare Agentur für Freiheit und Sicherheit
                 </p>
               </div>
               <button
