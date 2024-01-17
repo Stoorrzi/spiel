ee08f659cd1e992f0ec571856ec77b3bf1367bc0
Philipp Storz <philipp.storz@t-online.de>
2024-01-17 11:14:55 +0100

    update

--- src/Game/Prolog.js
+++ src/Game/Prolog.js
@@ -4,7 +4,7 @@ export default function Prolog({setProlog, weiter}) {
     return (
             <div className="D11">
               <div className="CoverDiv"></div>
-              <img className="IMG2" src={"/images/Mail.svg"} alt="prolog" />
+              <img className="IMG2" src={"/images/Prolog.png"} alt="prolog" />
               <div className="D12">
                 <h1 className="H11"><br /></h1>
                 <p className="P11"></p>
